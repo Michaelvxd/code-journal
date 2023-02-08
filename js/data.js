f3dc8a1d61c19063303dbd126fd1359bc868aeff
Michael Van Doorn <md@clickup.com>
2023-02-08 05:13:55 +0000

    Issue 1 - Complete

--- js/data.js
+++ js/data.js
@@ -1,8 +1,15 @@
 /* exported data */
-
 var data = {
   view: 'entry-form',
   entries: [],
   editing: null,
   nextEntryId: 1
 };
+
+window.addEventListener('beforeunload', function () {
+  localStorage.setItem('data', JSON.stringify(data));
+});
+
+if (localStorage.getItem('data')) {
+  data = JSON.parse(localStorage.getItem('data'));
+}
